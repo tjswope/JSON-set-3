b187cb702f1d0349db9fdafb07079ae686d67f79
Tom Swope <tjswope@gmail.com>
2022-12-12 08:59:57 -0500

    Initial Commit

---
+++ JSON/JSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONApp.swift
+//  JSON
+//
+//  Created by Swope, Thomas on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct JSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
